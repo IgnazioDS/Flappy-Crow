53624fae2499ddde68181146cf2e55546a12b903
Ignazio De Santis <ignaziodesantisofficial@gmail.com>
2026-01-21 22:59:45 +1000

    chore: set ios bundle id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.yourorg.flappycrow',
+  appId: 'com.ignaziods.flappycrow',
   appName: 'Flappy Crow',
   webDir: 'dist',
   bundledWebRuntime: false,
